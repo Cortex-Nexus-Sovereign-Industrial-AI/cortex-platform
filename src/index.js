6cd7812eb337d28a04432f57d668211c7eb73f0d
Michael Morim <mikecomplexai@gmail.com>
2026-07-18 08:28:53 +0100

    Add src/index.js placeholder for Node.js build

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Cortex Platform - Main Entry Point
+console.log('Cortex Platform initialized');
+
+export default {
+  version: '1.0.0',
+  name: 'cortex-platform',
+  init: () => {
+    console.log('Initializing Cortex Platform...');
+  }
+};
