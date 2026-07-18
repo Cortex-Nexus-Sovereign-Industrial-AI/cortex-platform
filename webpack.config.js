a07b4e3de4a66d28025bee719868aeff9fcef71e
Michael Morim <mikecomplexai@gmail.com>
2026-07-18 08:26:48 +0100

    Add Webpack configuration for project bundling

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: './src/index.js',
+  output: {
+    filename: 'cortex-platform.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'CortexPlatform',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
