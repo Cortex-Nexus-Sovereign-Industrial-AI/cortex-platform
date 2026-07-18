4c2f61db12bacbdab6809aabf46a3e8de976f772
Michael Morim <mikecomplexai@gmail.com>
2026-07-18 08:29:34 +0100

    Add Jest configuration for Node.js testing

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+export default {
+  testEnvironment: 'node',
+  collectCoverageFrom: ['src/**/*.js'],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+};
